2f0aa99ace686342d71c7f5178bbc7bb5e8157b3
Benn McAvoy <db@dbenn.com>
2024-12-30 14:48:22 +0000

    String ends with

---
+++ CodeChallenges/CodeWars/StringEndsWith.cs
@@ -0,0 +1,8 @@
+public class StringEndsWith
+{
+    /*
+    Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string). 
+    */
+    public static bool DoesStringEndWith(string str, string ending) =>
+        str.EndsWith(ending);
+}
